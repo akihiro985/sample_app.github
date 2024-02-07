3d3309a7af931423aa092f7fa67a54294ed7d2cc
akihiro985 <158790997+akihiro985@users.noreply.github.com>
2024-02-07 15:02:27 +0900

    Add files via upload

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,31 @@ class PostsController < ApplicationController
   
   def create
     @post = Post.new(content: params[:content])
-    @post.save
+    if @post.save
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
+  end
+  
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
+  end
+  
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
     redirect_to("/posts/index")
   end
 end
